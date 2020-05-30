5a8353321fa56550ad9bd4d7dfe9915623125ede
Joana Teixeira <joanajteixeira@outlook.com>
2020-05-30 09:55:14 +0100

    Updates

---
+++ precache-manifest.d8915ec47909b212eb7b624c07fcfc84.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "322f7af0cb09cd9b9a78cc18a25b330a",
+    "url": "/fictional/index.html"
+  },
+  {
+    "revision": "5f6c001fdd94e940b599",
+    "url": "/fictional/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "88590dbce55d97e2ea91",
+    "url": "/fictional/static/css/main.d61887ad.chunk.css"
+  },
+  {
+    "revision": "5f6c001fdd94e940b599",
+    "url": "/fictional/static/js/2.9ec9d3a0.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/fictional/static/js/2.9ec9d3a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88590dbce55d97e2ea91",
+    "url": "/fictional/static/js/main.7031479a.chunk.js"
+  },
+  {
+    "revision": "bfc408c1cd8484638c0b",
+    "url": "/fictional/static/js/runtime-main.4a1f9028.js"
+  }
+]);
